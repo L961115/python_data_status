bcef04b198c9e90e80f8465df6d2a1bf6535a8a1
Blog <862418977@qq.com>
2020-03-02 11:34:11 +0800

    算法分析，大O符号

---
+++ practise2.py
@@ -0,0 +1,21 @@
+"""
+T（n）= 3*n^2 + 2n +4
+只去变化最快的
+O(n^2)
+"""
+
+a = 100
+b = 500  # 3
+c = 400
+
+for i in range (n):  
+    for j in range(n):  # 3*n^2(3：三个变量，n^2:两个循环)
+        d = a+100
+        e = b^2
+        f = i*j
+
+for k in ramge(n):  # 2n 一个循环两个变量
+    y = a*b
+    v = b*b
+
+h = 500  #1
