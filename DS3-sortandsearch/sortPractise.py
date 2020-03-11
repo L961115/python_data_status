b0b3ed952c6167bda7a65133ff18fb6e919322b2
Blog <862418977@qq.com>
2020-03-11 16:13:51 +0800

    归并排序和快速排序

--- DS3-sortandsearch/sortPractise.py
+++ DS3-sortandsearch/sortPractise.py
@@ -20,90 +20,220 @@
 '''
 
 
-def bubbleSort(alist):
-    for passnum in range(len(alist)-1,0,-1):
-        for i in range(passnum):
-            if alist[i] > alist[i+1]:  #倒序变为 <
-                temp = alist[i]
-                alist[i] = alist[i+1]
-                alist[i+1] =temp
-alist= [54,26,93,17,77,31,44,55,20] 
-bubbleSort(alist)
-print(alist)
+# def bubbleSort(alist):
+#     for passnum in range(len(alist)-1,0,-1):
+#         for i in range(passnum):
+#             if alist[i] > alist[i+1]:  #倒序变为 <
+#                 temp = alist[i]
+#                 alist[i] = alist[i+1]
+#                 alist[i+1] =temp
+# alist= [54,26,93,17,77,31,44,55,20] 
+# bubbleSort(alist)
+# print(alist)
 
 
-#短冒泡排序
-def bubbleSort(alist):
-    flag = True
-    passnum = len(alist) - 1
+# #短冒泡排序
+# def bubbleSort(alist):
+#     flag = True
+#     passnum = len(alist) - 1
 
 
-    while flag and passnum>0:
-        flag = False
-        for i in range (passnum):
-            if alist[i] > alist[i+1]:  #倒序变为 <
-                temp = alist[i]
-                alist[i] = alist[i+1]
-                alist[i+1] =temp
-                flag = True
-alist= [54,26,93,17,77,31,44,55,20] 
-bubbleSort(alist)
-print(alist)
+#     while flag and passnum>0:
+#         flag = False
+#         for i in range (passnum):
+#             if alist[i] > alist[i+1]:  #倒序变为 <
+#                 temp = alist[i]
+#                 alist[i] = alist[i+1]
+#                 alist[i+1] =temp
+#                 flag = True
+# alist= [54,26,93,17,77,31,44,55,20] 
+# bubbleSort(alist)
+# print(alist)
 
 
-#选择排序
-def selectionSort(alist):
-    for fillsort in range(len(alist)-1,0,-1):
-        maxPos = 0
-        for location in range(1,fillsort+1):
-            if alist[location] > alist[maxPos]:
-                maxPos = location
+# #选择排序
+# def selectionSort(alist):
+#     for fillsort in range(len(alist)-1,0,-1):
+#         maxPos = 0
+#         for location in range(1,fillsort+1):
+#             if alist[location] > alist[maxPos]:
+#                 maxPos = location
 
-        temp = alist[fillsort]
-        alist[fillsort] = alist[maxPos]
-        alist[maxPos] = temp
+#         temp = alist[fillsort]
+#         alist[fillsort] = alist[maxPos]
+#         alist[maxPos] = temp
 
-alist= [54,26,93,17,77,31,44,55,20] 
-selectionSort(alist)
-print(alist)
+# alist= [54,26,93,17,77,31,44,55,20] 
+# selectionSort(alist)
+# print(alist)
+
+
+# '''
+#     插入排序
+# '''
+# def insertSort(alist):
+#     for i in range(1,len(alist)):
+#         currenValue = alist[i]
+#         while i > 0 and alist[i - 1] > currenValue:
+#             alist[i] = alist[i - 1]
+#             i = i - 1
+
+#         alist[i]  =currenValue
+
+# alist = [54,26,93,17,77,31,44,55,20] 
+# insertSort(alist)
+# print(alist)
+
+
+# '''
+#     希尔排序
+# '''
+# def shellSort(alist):
+#     sublistcount = len(alist) // 2
+
+#     while sublistcount>0:
+#         for startposition in range(sublistcount):
+#             gapInsertionSort(alist,startposition,sublistcount)
+
+#         print('alist:',alist)
+
+#         sublistcount = sublistcount // 2
+
+# def gapInsertionSort(alist,start,gap):
+#     for i in range(start,len(alist),gap):
+#         currenValue = alist[i]
+#         while i >= gap and alist[i - 1] > currenValue:
+#             alist[i] = alist[i - 1]
+#             i = i - 1
+
+#         alist[i]  =currenValue
+
+
 
 
 '''
-    插入排序
+    归并排序
 '''
-def insertSort(alist):
-    for i in range(1,len(alist)):
-        currenValue = alist[i]
-        while i > 0 and alist[i - 1] > currenValue:
-            alist[i] = alist[i - 1]
-            i = i - 1
+# def mergeSort(alist):
+#     print('拆分的列表：',alist)
+#     if len(alist) > 1:
+#         mid = len(alist) // 2
+#         leftHalf = alist[:mid]
+#         rightHalf = alist[mid:]
 
-        alist[i]  =currenValue
+#         mergeSort(leftHalf)
+#         mergeSort(rightHalf)
 
-alist = [54,26,93,17,77,31,44,55,20] 
-insertSort(alist)
+#         i = 0
+#         j = 0
+#         k = 0
+#         while i < len(leftHalf) and j < len(rightHalf):
+#             if leftHalf[i] < rightHalf[i]:
+#                 alist[k] = leftHalf[i]
+#                 i = i + 1
+#             else:
+#                 alist[k] = rightHalf[j]
+#                 j = j + 1
+
+#             k = k + 1
+#         while i < len(leftHalf):
+#             alist[k] = leftHalf[i]
+#             i = i + 1
+#             k = k + 1
+#         while j < len(rightHalf):
+#             alist[k] = rightHalf[j]
+#             j = j + 1
+#             k = k + 1
+#     print('合并：',alist)
+
+# alist = [54,26,93,17,77,31,44,55,20] 
+# mergeSort(alist)
+# print(alist)
+
+# def mergeSort(alist):
+#     print('拆分的列表',alist)
+#     if len(alist) > 1:
+#         mid = len(alist)//2
+#         leftHalf = alist[:mid]
+#         rightHalf = alist[mid:]
+
+#         mergeSort(leftHalf)
+#         mergeSort(rightHalf)
+
+#         i = 0
+#         j = 0
+#         k = 0
+#         while i < len(leftHalf) and j < len(rightHalf):
+#             if leftHalf[i] < rightHalf[j]:
+#                 alist[k] = leftHalf[i]
+#                 i = i + 1
+#             else:
+#                 alist[k] = rightHalf[j]
+#                 j = j + 1
+            
+#             k = k + 1
+#         while i < len(leftHalf):
+#             alist[k] = leftHalf[i]
+#             i = i + 1
+#             k = k + 1
+#         while j < len(rightHalf):
+#             alist[k] = rightHalf[j]
+#             j = j + 1
+#             k = k + 1
+#     print('合并：',alist)
+
+# alist = [54,26,93,17,77,31,44,55,20]
+# mergeSort(alist)
+# print(alist)
+
+
+
+'''
+    快速排序
+'''
+def quickSort(alist):
+    quickSortHelper(alist,0,len(alist)-1)
+
+
+# 递归调用对数列进行分区
+def quickSortHelper(alist,first,last):
+    if first < last:
+        splitpoint = partition(alist,first,last)
+
+        quickSortHelper(alist,first,splitpoint-1)
+        quickSortHelper(alist,splitpoint+1,last)
+
+# 选出基准点
+def partition(alist,first,last):
+    # 定义基准点
+    pivotvalue = alist[first]
+
+    leftMark = first + 1
+    rightMark = last
+
+    # 停止比对
+    done = False
+
+    while not done:
+        while leftMark <= rightMark and alist[leftMark] <= pivotvalue:
+            leftMark = leftMark + 1
+
+        while alist[rightMark] >= pivotvalue and rightMark >= leftMark:
+            rightMark = rightMark - 1
+
+        if rightMark < leftMark:
+            done = True
+        else:
+            temp = alist[leftMark]
+            alist[leftMark] = alist[rightMark]
+            alist[rightMark] = temp
+
+    temp = alist[first]
+    alist[first] = alist[rightMark]
+    alist[rightMark] = temp
+
+    return rightMark
+
+alist = [54,26,93,17,77,31,44,55,20]
+quickSort(alist)
 print(alist)
-
-
-'''
-    希尔排序
-'''
-def shellSort(alist):
-    sublistcount = len(alist) // 2
-
-    while sublistcount>0:
-        for startposition in range(sublistcount):
-            gapInsertionSort(alist,startposition,sublistcount)
-
-        print('alist:',alist)
-
-        sublistcount = sublistcount // 2
-
-def gapInsertionSort(alist,start,gap):
-    for i in range(start,len(alist),gap):
-        currenValue = alist[i]
-        while i >= gap and alist[i - 1] > currenValue:
-            alist[i] = alist[i - 1]
-            i = i - 1
-
-        alist[i]  =currenValue
